047b161174f162715520f00d249d12d1cc2d3544
Punith839 <kpunith675@gmail.com>
2024-12-21 15:14:21 +0530

    done

---
+++ sssttt.py
@@ -0,0 +1,11 @@
+#ptr2=open("names.txt",'r')
+#res2=ptr2.read(6)
+#print(res2)
+
+ptr3=open("file.txt",'r')
+res3=ptr3.readline()
+print(res3)
+
+ptr4=open("file.txt",'r')
+res4=ptr4.readlines()
+print(res4)
